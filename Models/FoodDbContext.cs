080f9516f6d743e5975274c25340c9597fabc236
Anh.D.Nguyen <117455080+DucAnhETERNAL@users.noreply.github.com>
2024-11-06 08:55:40 +0700

    New Repo

---
+++ Models/FoodDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class FoodDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .Build();
+
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("Food"));
+            }
+
+            optionsBuilder.EnableSensitiveDataLogging(); // Optional: chỉ sử dụng trong môi trường phát triển
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Tùy chỉnh các cấu hình khác (nếu cần)
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Username { get; set; }
+
+        [MaxLength(15)]
+        public string Mobile { get; set; }
+
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        [MaxLength(10)]
+        public string PostCode { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [MaxLength(500)]
+        public string ImageUrl { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public ICollection<Cart> Carts { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
