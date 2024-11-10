fb9037b278bf0460ffd9feaab850a42107bf9d2e
Anh.D.Nguyen <117455080+DucAnhETERNAL@users.noreply.github.com>
2024-11-11 00:14:47 +0700

    Testing feature purchase

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e046e3b455dde24372c07d4713b7504a6cd198")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9ae33751d45f555c05cdd775a42db95bb01a819")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
