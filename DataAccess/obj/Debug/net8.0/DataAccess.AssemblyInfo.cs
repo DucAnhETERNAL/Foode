1c23868c57469630389232d252896c1d3962a251
Nam <156413717+kaizy2@users.noreply.github.com>
2024-11-10 18:15:40 +0700

    Update

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e046e3b455dde24372c07d4713b7504a6cd198")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d85e38c93875e2ef925f39acc9e7578d0c02f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
