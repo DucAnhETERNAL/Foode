aa64f41aace5020f34022c0437cc17d1b006568c
Sueluck1 <136314227+Sueluck1@users.noreply.github.com>
2024-11-11 13:28:31 +0700

    nice

--- Food/obj/Debug/net8.0/Food.AssemblyInfo.cs
+++ Food/obj/Debug/net8.0/Food.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Food")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d85e38c93875e2ef925f39acc9e7578d0c02f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70a9952b50a9a86dc4553ed3e0bdf9a720a782f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Food")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Food")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d85e38c93875e2ef925f39acc9e7578d0c02f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70a9952b50a9a86dc4553ed3e0bdf9a720a782f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
